7e6ea56e46df57ca5d51bd956396ac3fb0871d87
rcorenti <corentin.reyleroux@ynov.com>
2025-03-17 11:52:17 +0100

    Add todo.py

---
+++ todo.py
@@ -0,0 +1,22 @@
+# TODO: Battleships
+
+# TODO: Logique du jeu :
+#           - a faire...
+
+# TODO: Interface graphique :
+#           - Interface joueur :
+#               - Deux tableau (défense et attaque)
+#             ? - bateau ennemi -> restants/touchés/coulés
+#           - Menu : a définir
+#           - autre ?
+
+
+# TODO: Analyser des données pour l'IA :
+#           - logique de début de partie (placement des bateau)
+#           - logique d'attaque
+#               - si pas touché -> - ?
+#               - si toucher -> jouer a coté
+#                       - si re toucher -> +
+#                       - sinon -> -
+#               - si coulé -> ++
+#           - autre ?
