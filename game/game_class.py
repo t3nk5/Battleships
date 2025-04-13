f6463de71cc430a449479f52dfc1da5d280bdff8
verwann <erwann.varlet@ynov.com>
2025-04-13 21:13:14 +0200

    separate unique game turn

--- game/game_class.py
+++ game/game_class.py
@@ -6,24 +6,10 @@ from utils.prompt import Prompt, clear
 
 
 class Game:
-    def __init__(self, mode: Literal['PvP', 'PvIA', 'IAvIA']):
+    def __init__(self, mode: Literal['PvP', 'PvIA', 'IAvIA', 'Test']):
         self.mode = mode
         self.players: list[Player] = []
         self._player_index = 0
-
-        match self.mode:
-            case 'PvP':
-                self.players.extend([
-                    Player(Prompt.get('Enter player 1 name: ', expected_type=str)),
-                    Player(Prompt.get('Enter player 2 name: ', expected_type=str)),
-                ])
-            case 'PvIA':
-                raise ValueError('Not implemented yet')
-            case 'IAvIA':
-                raise ValueError('Not implemented yet')
-            case _:
-                raise ValueError(f'Unknown mode: {self.mode}')
-
     @property
     def current_player(self) -> Player:
         return self.players[self._player_index]
@@ -37,6 +23,21 @@ class Game:
         return self.players[self._next_index]
 
     def initiate(self):
+        match self.mode:
+            case 'PvP':
+                self.players.extend([
+                    Player(Prompt.get('Enter player 1 name: ', expected_type=str)),
+                    Player(Prompt.get('Enter player 2 name: ', expected_type=str)),
+                ])
+            case 'PvIA':
+                raise ValueError('Not implemented yet')
+            case 'IAvIA':
+                raise ValueError('Not implemented yet')
+            case 'Test':
+                pass
+            case _:
+                raise ValueError(f'Unknown mode: {self.mode}')
+
         clear(1)
         for player in self.players:
             player.initiate_grid()
@@ -46,15 +47,18 @@ class Game:
         clear(1)
         print(f'Grids initialized !')
 
+    def turn(self):
+        clear(2)
+        print(f'Player {self.current_player.name} turn:\n')
+        print(self.current_player)
+        print()
+
+        result = self.current_player.shot(self.next_player)
+        print(('Touched' + (', Sunk' if result.sunken else '') if result.touched else 'Missed') + ' !')
+
     def play(self):
         while True:
-            clear(2)
-            print(f'Player {self.current_player.name} turn:\n')
-            print(self.current_player)
-            print()
-
-            result = self.current_player.shot(self.next_player)
-            print(('Touched' + (', Sunk' if result.sunken else '') if result.touched else 'Missed') + ' !')
+            self.turn()
 
             if not self.next_player.defensive_grid.alive: break
 
